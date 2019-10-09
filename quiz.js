7ae81c4304aff4faa24346d3bda830cb4915a9b0
Maria Frøsig Christensen <mari43fe@stud.kea.dk>
2019-10-09 17:35:05 +0200

    quiz virker

--- quiz.js
+++ quiz.js
@@ -1,111 +1,106 @@
-// https://www.codeexplained.org/2018/10/create-multiple-choice-quiz-using-javascript.html
-
-const start = document.getElementById("start");
-
-const quiz = document.getElementById("quiz");
-
-const question = document.getElementById("question");
-
-const qImg = document.getElementById("qImg");
-
-const choiceA = document.getElementById("A");
-
-const choiceB = document.getElementById("B");
-
-const choiceC = document.getElementById("C");
-
-const counter = document.getElementById("counter");
-
-const timeGauge = document.getElementById("timeGauge");
-
-const progress = document.getElementById("progress");
-
-const scoreDiv = document.getElementById("scoreContainer");
-
-let questions = [
-
-    {
-
-        question: "What does HTML stand for?",
-
-        imgSrc: "img/html.png",
-
-        choiceA: "Correct",
-
-        choiceB: "Wrong",
-
-        choiceC: "Wrong",
-
-        correct: "A"
-
-    }, {
-
-        question: "What does CSS stand for?",
-
-        imgSrc: "img/css.png",
-
-        choiceA: "Wrong",
-
-        choiceB: "Correct",
-
-        choiceC: "Wrong",
-
-        correct: "B"
-
-    }, {
-
-        question: "What does JS stand for?",
-
-        imgSrc: "img/js.png",
-
-        choiceA: "Wrong",
-
-        choiceB: "Wrong",
-
-        choiceC: "Correct",
-
-        correct: "C"
-
-    }
-
-];
+window.addEventListener("load", showQuiz);
 
 
-const lastQuestion = questions.length - 1;
+function showQuiz() {
+    console.log("siden vises!");
+    document.querySelector("#button_start").addEventListener("click", startQuiz);
 
-let runningQuestion = 0;
-
-
-// render a question
-
-function renderQuestion() {
-
-    let q = questions[runningQuestion];
-
-
-
-    question.innerHTML = "<p>" + q.question + "</p>";
-
-    qImg.innerHTML = "<img src=" + q.imgSrc + ">";
-
-    choiceA.innerHTML = q.choiceA;
-
-    choiceB.innerHTML = q.choiceB;
-
-    choiceC.innerHTML = q.choiceC;
 
 }
 
-// render progress
+function startQuiz() {
+    console.log("quiz starter");
 
-function renderProgress(){
+    document.querySelector("#button_start").classList.add("hide");
 
-    for(let qIndex = 0; qIndex <= lastQuestion; qIndex++){
+    document.querySelector("#question1").classList.remove("hide");
+    
+    
 
-        progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
+    document.querySelector("#YES1").addEventListener("click", question2);
+    document.querySelector("#NO1").addEventListener("click", question2);
 
-    }
 
 }
 
-start.addEventListener("click",startQuiz);
+
+function question2() {
+    console.log("question2");
+
+    document.querySelector("#question1").classList.add("hide");
+    document.querySelector("#question2").classList.remove("hide");
+
+
+  document.querySelector("#YES2").addEventListener("click", question3);
+    document.querySelector("#NO2").addEventListener("click", question3);
+
+}
+
+
+function question3() {
+    console.log("question3");
+
+    document.querySelector("#question2").classList.add("hide");
+    document.querySelector("#question3").classList.remove("hide");
+
+
+     document.querySelector("#YES3").addEventListener("click", result);
+    document.querySelector("#NO3").addEventListener("click", result);
+
+
+}
+
+
+function result() {
+    console.log("resultat");
+
+    document.querySelector("#question3").classList.add("hide");
+    document.querySelector("#result").classList.remove("hide");
+
+
+    document.querySelector("#again").addEventListener("click", restart);
+
+
+}
+
+function restart() {
+    
+       document.querySelector("#result").classList.add("hide");
+    
+    startQuiz();
+    
+}
+
+
+//     
+//    document.querySelector("#menu").classList.toggle("hidden");
+//
+// 	let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+//
+// 	if (erSkjult == true) {
+// 		//menuen er nu skjult - ændr menuknap til lll
+// 		document.querySelector("#menuknap").textContent = "☰";
+// 		document.querySelector(".grid-container").classList.remove("hidden");
+// 		document.querySelector("#menuknap").classList.remove("menuknapned");
+//
+// 		// tilføje scroll igen
+// 		document.querySelector("body").classList.remove("overflow")
+// 		document.querySelector(".burgermenu").classList.remove("background");;
+// 		document.querySelector("#menuknap").classList.remove("menuknapnormal");
+//
+// 		document.querySelector("#menuknap").classList.add("menuknapned");
+//
+//
+// 	} else {
+// 		//menuen er nu vist - ændr menuknap til x
+// 		document.querySelector("#menuknap").textContent = "X"
+// 		document.querySelector(".grid-container").classList.add("hidden");
+//
+// 		// fjerne muligheden for at scrolle
+// 		document.querySelector("body").classList.add("overflow");
+//
+// 		document.querySelector(".burgermenu").classList.add("background");
+// 		document.querySelector("#menuknap").classList.add("menuknapnormal");
+// 		document.querySelector("#menuknap").classList.remove("menuknapned");
+// 	}
+//
